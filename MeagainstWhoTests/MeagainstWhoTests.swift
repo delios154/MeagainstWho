a7422fd44d9978a88edb864cff8a02b2da00d5b3
delios154 <almansoori.154@gmail.com>
2025-09-27 09:32:47 +0400

    Initial Commit

---
+++ MeagainstWhoTests/MeagainstWhoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeagainstWhoTests.swift
+//  MeagainstWhoTests
+//
+//  Created by Mohammed Almansoori on 27/09/2025.
+//
+
+import Testing
+@testable import MeagainstWho
+
+struct MeagainstWhoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
